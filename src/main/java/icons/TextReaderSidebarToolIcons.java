a912756f405f7f02163c4352d6878803348ed088
MUedsa <7676275+muedsa@users.noreply.github.com>
2021-11-19 14:29:06 +0800

    update: name and icon

---
+++ src/main/java/icons/TextReaderSidebarToolIcons.java
@@ -0,0 +1,10 @@
+package icons;
+
+import com.intellij.openapi.util.IconLoader;
+
+import javax.swing.*;
+
+
+public interface TextReaderSidebarToolIcons {
+    Icon BOOK_DEAD = IconLoader.getIcon("/icons/bookDead.svg", TextReaderSidebarToolIcons.class);
+}
