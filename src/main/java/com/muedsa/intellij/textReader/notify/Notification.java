e0129d045b49a5ca790853b0a42b82274969c338
MUedsa <7676275+MUedsa@users.noreply.github.com>
2021-12-06 15:19:37 +0800

    update: lower idea version -> 2020.1

--- src/main/java/com/muedsa/intellij/textReader/notify/Notification.java
+++ src/main/java/com/muedsa/intellij/textReader/notify/Notification.java
@@ -1,21 +1,27 @@
 package com.muedsa.intellij.textReader.notify;
 
-import com.intellij.notification.NotificationGroupManager;
+import com.intellij.notification.NotificationDisplayType;
+import com.intellij.notification.NotificationGroup;
 import com.intellij.notification.NotificationType;
 import com.intellij.openapi.project.Project;
 
 public class Notification {
 
+    private static final NotificationGroup NORMAL_Notify_GROUP = new NotificationGroup(
+            "com.muedsa.intellij.textReader.notify",
+            NotificationDisplayType.BALLOON,
+            true);
+
+    private static final NotificationGroup HIDDEN_NOTIFY_GROUP = new NotificationGroup(
+            "com.muedsa.intellij.textReader.hiddenNotify",
+            NotificationDisplayType.BALLOON,
+            true);
+
     public static void sendNotify(Project project, String title, String content, NotificationType type){
-        NotificationGroupManager.getInstance().getNotificationGroup("com.muedsa.intellij.textReader.notify")
-                .createNotification(content, type)
-                .setTitle(title)
-                .notify(project);
+        NORMAL_Notify_GROUP.createNotification(content, type).setTitle(title).notify(project);
     }
 
     public static void sendHiddenNotify(Project project, String content, NotificationType type){
-        NotificationGroupManager.getInstance().getNotificationGroup("com.muedsa.intellij.textReader.hiddenNotify")
-                .createNotification(content, type)
-                .notify(project);
+        HIDDEN_NOTIFY_GROUP.createNotification(content, type).notify(project);
     }
 }
