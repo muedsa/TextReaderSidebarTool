084cfbf613ad8e69c6e7dfbc51c5c3254144d8f5
MUEDSA <7676275+muedsa@users.noreply.github.com>
2025-02-24 15:07:35 +0800

    chore: 移除不必要的代码

--- src/main/java/com/muedsa/intellij/textReader/action/EmptyLineAction.java
+++ src/main/java/com/muedsa/intellij/textReader/action/EmptyLineAction.java
@@ -2,14 +2,12 @@ package com.muedsa.intellij.textReader.action;
 
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
-import com.muedsa.intellij.textReader.state.TextReaderConfigStateService;
 import com.muedsa.intellij.textReader.util.ReaderLineUtil;
 import org.jetbrains.annotations.NotNull;
 
 public class EmptyLineAction extends AnAction {
     @Override
     public void actionPerformed(@NotNull AnActionEvent e) {
-        TextReaderConfigStateService config = TextReaderConfigStateService.getInstance();
         ReaderLineUtil.clearLast();
     }
 }
